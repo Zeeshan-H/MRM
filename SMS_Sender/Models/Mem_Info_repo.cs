c650f84c6205e2edee050760cf295be220ecaa67
Zeeshan-H <zeeshanh184@gmail.com>
2019-11-19 16:55:00 -0800

    Up

---
+++ SMS_Sender/Models/Mem_Info_repo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class Mem_Info_repo
+    {
+
+        public IQueryable<Mem_Info> GetAllMemInfo()
+        {
+            messsageEntities entities = new messsageEntities();
+            return entities.Mem_Info;
+ 
+        }
+
+        public IQueryable<Mem_Info> GetAllMemInfo_Byid(int id)
+        {
+
+            messsageEntities entities = new messsageEntities();
+            return entities.Mem_Info.Where(c => c.MemId == id).Select(e => e);
+        }
+    }
+}
---
+++ SMS_Sender/Models/Mem_TitleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class Mem_TitleModel
+    {
+
+        public int TitleId { get; set; }
+
+        public string TitileDesc { get; set; }
+
+
+
+    }
+}
