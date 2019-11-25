3cfc6652bac5c8b781b953639e14a2150a1220a0
Zeeshan-H <zeeshanh184@gmail.com>
2019-11-25 11:43:09 -0800

    Upload

---
+++ SMS_Sender/Models/Gen_UserStatusModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class Gen_UserStatusModel
+    {
+        public short UserStatusId { get; set; }
+        public string UserStatus { get; set; }
+
+        public IEnumerable<Mem_MemberToGroupModel> Mem_MemberToGroupModel { get; set; }
+    }
+}
