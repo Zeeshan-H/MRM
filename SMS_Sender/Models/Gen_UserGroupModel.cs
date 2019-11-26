f413e27f6627176a298a22bad030da4db7e690ec
Zeeshan-H <zeeshanh184@gmail.com>
2019-11-25 17:18:59 -0800

    Up

---
+++ SMS_Sender/Models/Gen_UserGroupModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class Gen_UserGroupModel
+    {
+        public short GrpId { get; set; }
+        public short MemTypeId { get; set; }
+        public string UserGrp { get; set; }
+
+        public IEnumerable<Mem_MemberToGroupModel> Mem_MemberToGroupModel { get; set; }
+    }
+}
---
+++ SMS_Sender/Models/UserKey.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class UserKey
+    {
+
+        public int MemId { get; set; }
+
+        public short MemTypeId { get; set; }
+
+        public short TitleId { get; set; }
+        public string TitleDesc { get; set; }
+
+        public string ParentMemID { get; set; }
+
+        public string FName { get; set; }
+
+        public string LName { get; set; }
+
+        public string PrimaryNo { get; set; }
+
+        public string EmailAdd { get; set; }
+
+        public int MemGrpId { get; set; }
+
+        public string UserGrp { get; set; }
+
+        public int MFId { get; set; }
+
+
+    }
+}
