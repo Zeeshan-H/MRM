a0c605e4ea62b139c4818ad1ccb870b848e6cb43
Zeeshan-H <zeeshanh184@gmail.com>
2019-11-25 17:17:19 -0800

    del

--- SMS_Sender/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace SMS_Sender.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
