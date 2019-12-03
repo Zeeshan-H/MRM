1e8c1aec9ffc66d6784a92a3443fadd20229d3ba
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-03 12:06:19 -0800

    Upload

---
+++ SMS_Sender/Controllers/get_mhController.cs
@@ -0,0 +1,42 @@
+﻿using SMS_Sender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SMS_Sender.Controllers
+{
+    public class get_mhController : ApiController
+    {
+        [HttpGet]
+
+        public IHttpActionResult Get(int memid)
+        {
+
+            int flag = 0;
+            mrmdbEntities entities = new mrmdbEntities();
+
+            MH_MedicalHistoryModel model = new MH_MedicalHistoryModel();
+
+            List<usp_MH_GetAllHistorySecs_Result> list = new List<usp_MH_GetAllHistorySecs_Result>();
+
+            list = entities.usp_MH_GetAllHistorySecs(memid).ToList();
+
+            model.BloodGroup = list.ElementAt(0).BloodGroup;
+            model.MedCondition = list.ElementAt(0).MedCondition;
+            model.MedProcedure = list.ElementAt(0).MedProcedure;
+            model.MedAllergy = list.ElementAt(0).MedAllergy;
+            model.FamilyCond = list.ElementAt(0).FamilyCond;
+
+            if (list.ElementAt(0).BloodGroup == null)
+            {
+                return Ok(flag);
+            }
+            else
+                return Ok(model);
+        
+        }
+    }
+}
---
+++ SMS_Sender/Controllers/get_mh_medcondController.cs
@@ -0,0 +1,45 @@
+﻿using SMS_Sender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SMS_Sender.Controllers
+{
+    public class get_mh_medcondController : ApiController
+    {
+        [HttpPost]
+        [ResponseType(typeof(MH_Mem_MedConditionModel))]
+        public IHttpActionResult Post(MH_Mem_MedConditionModel model)
+        {
+
+            int flag = 1;
+            mrmdbEntities db = new mrmdbEntities();
+            MH_Mem_MedCondition cond = new MH_Mem_MedCondition();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+            cond.MemMedCond = model.MemMedCond;
+            cond.MemId = model.MemId;
+            cond.MedCondId = model.MedCondId;
+            cond.TPId = model.TPId;
+            cond.AddedBy = model.AddedBy;
+            cond.AddedOn = DateTime.Now;
+            cond.UpdateBy = model.UpdateBy;
+            cond.UpdateOn = DateTime.Now;
+            cond.IsDel = false;
+
+            db.MH_Mem_MedCondition.Add(cond);
+            db.SaveChanges();
+
+            return Ok(flag);
+        }
+
+
+    }
+}
+
