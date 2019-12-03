997806227d84bd6a3c94cc1658c1756956b71396
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-03 13:03:51 -0800

    Up

---
+++ SMS_Sender/Controllers/get_mh_bloodgroupController.cs
@@ -0,0 +1,51 @@
+﻿using SMS_Sender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SMS_Sender.Controllers
+{
+    public class get_mh_bloodgroupController : ApiController
+    {
+        [HttpPost]
+        [ResponseType(typeof(BloodGroupModel))]
+        public IHttpActionResult Post(BloodGroupModel model)
+        {
+            int flag = 1;
+            mrmdbEntities db = new mrmdbEntities();
+            MH_Mem_BloodGroup bg = new MH_Mem_BloodGroup();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            bg.MemBGId = model.MemBGId;
+            bg.MemId = model.MemId;
+            bg.BGroup = model.BGroup;
+            bg.BGComments = model.BGComments;
+            bg.AddedBy = model.AddedBy;
+            bg.AddedOn = DateTime.Now;
+            bg.UpdateBy = model.UpdateBy;
+            bg.UpdateOn = DateTime.Now;
+
+            db.MH_Mem_BloodGroup.Add(bg);
+            db.SaveChanges();
+
+            return Ok(flag);
+            //Updating Model
+            //model.MemBGId = bg.MemBGId;
+            //model.AddedBy = bg.AddedBy;
+            //model.AddedOn = bg.AddedOn;
+            //model.UpdateBy = bg.UpdateBy;
+            //model.UpdateOn = bg.UpdateOn;
+
+            //return CreatedAtRoute("DefaultApi", new
+            //    {
+            //        id = model.MemBGId
+            //    }, model);
+
+        }
+    }
+}
---
+++ SMS_Sender/Models/UserKey.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class UserKey
+    {
+
+        public int MemId { get; set; }
+
+        public short MemTypeId { get; set; }
+
+        public short TitleId { get; set; }
+        public string TitleDesc { get; set; }
+
+        public string ParentMemID { get; set; }
+        [StringLength(100)]
+        public string FName { get; set; }
+
+        [StringLength(100)]
+        public string LName { get; set; }
+
+        public string PrimaryNo { get; set; }
+
+        public string EmailAdd { get; set; }
+
+        public int MemGrpId { get; set; }
+
+        public string UserGrp { get; set; }
+
+        public int MFId { get; set; }
+
+        public string Username { get; set; }
+
+    }
+}
