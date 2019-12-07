e09b9d4b9d46065679f091d9b475746f88dabd67
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-06 16:10:28 -0800

    Del

---
+++ SMS_Sender/Models/Gen_UserAccsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class Gen_UserAccsModel
+    {
+
+        public int UserId { get; set; }
+        public int MemGrpId { get; set; }
+        public string PassKey { get; set; }
+        public int AddedBy { get; set; }
+        public System.DateTime AddedOn { get; set; }
+        public int UpdateBy { get; set; }
+        public System.DateTime UpdateOn { get; set; }
+
+        
+
+    }
+}
