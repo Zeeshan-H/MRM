30b8492eecd41ab27b3f354f4d89d227d6997a47
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-06 17:02:32 -0800

    Up

---
+++ SMS_Sender/Models/MH_MedicalHistoryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class MH_MedicalHistoryModel
+    {
+        public string BloodGroup { get; set; }
+
+        public string MedCondition { get; set; }
+
+        public string MedProcedure { get; set; }
+
+        public string MedAllergy { get; set; }
+
+        public string FamilyCond { get; set; }
+    }
+}
---
+++ SMS_Sender/Models/MH_Mem_RoutineMedsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class MH_Mem_RoutineMedsModel
+    {
+        public int MemMedsId { get; set; }
+        public int MemId { get; set; }
+        public string MemMeds { get; set; }
+        public int AddedBy { get; set; }
+        public System.DateTime AddedOn { get; set; }
+        public int UpdateBy { get; set; }
+        public System.DateTime UpdateOn { get; set; }
+    }
+}
