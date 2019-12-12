7dbfb8f6f12aa33f8bec1b71056905e767ae930d
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-11 23:14:20 -0800

    Up

---
+++ SMS_Sender/Controllers/get_mf_detailsController.cs
@@ -0,0 +1,31 @@
+﻿using SMS_Sender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SMS_Sender.Controllers
+{
+    public class get_mf_detailsController : ApiController
+    {
+        ModelFactory modelfactory;
+
+        get_mf_detailsController() {
+
+            modelfactory = new ModelFactory();
+        }
+        public IEnumerable<MF_DetailsModel> Get()
+        {
+
+            MF_Detailsrepo mem_info_repo = new MF_Detailsrepo();
+
+            return mem_info_repo.GetAllMF_Details().ToList().Select(c => modelfactory.Create5(c));
+
+
+
+        }
+
+    }
+}
