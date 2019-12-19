758f3a7ec3a3dd71a640f9f714000ce9f383bff1
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-18 17:41:57 -0800

    Up

---
+++ SMS_Sender/Models/LookupMedCondModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class LookupMedCondModel
+    {
+
+        public int MedCondId { get; set; }
+    }
+}
