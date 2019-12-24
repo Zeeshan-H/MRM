ae39e04a2927804fb51cae3fb716b46a3e900020
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-24 12:48:44 -0800

    Up

---
+++ MRM/Models/MedicalHistoryModel5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class MedicalHistoryModel5
+    {
+        public int MemID { get; set; }
+
+        public int UserID { get; set; }
+
+        public string FamilyCond { get; set; }
+    }
+}
