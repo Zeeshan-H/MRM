414fc8f234cacb5e298ea12f80e9bce60373363a
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-24 18:16:32 -0800

    Del

--- MRM/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace SMS_Sender.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
