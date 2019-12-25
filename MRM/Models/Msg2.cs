82cec5b5644238c94bd02d9d619b21f89b1dc62d
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-24 16:40:40 -0800

    Up

---
+++ MRM/Models/Msg2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class Msg2
+    {
+        public int Id { get; set; }
+
+   //     public string mobile { get; set; }
+
+        public string msg { get; set; }
+
+        public string username { get; set; }
+    }
+}
