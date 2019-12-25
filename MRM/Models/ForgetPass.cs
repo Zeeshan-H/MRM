c55545efdd6ceef7a8cbdeb32e47dde15b397e6d
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-24 19:07:36 -0800

    Up

---
+++ MRM/Models/ForgetPass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class ForgetPass
+    {
+        public int UserId { get; set; }
+
+        public int MemId { get; set; }
+    }
+}
---
+++ MRM/Models/Gen_UserStatusrepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class Gen_UserStatusrepo
+    {
+        public IQueryable<Gen_UserStatus> GetAllGen_UserStatus()
+        {
+            mrmdbEntities entities = new mrmdbEntities();
+            return entities.Gen_UserStatus;
+
+        }
+
+        public IQueryable<Gen_UserStatus> GetAllGen_UserStatusById(int id)
+        {
+
+            mrmdbEntities entities = new mrmdbEntities();
+            return entities.Gen_UserStatus.Where(c => c.UserStatusId == id).Select(e => e);
+        }
+    }
+}
