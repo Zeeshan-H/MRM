e6f5030ffc7cf142621f6d3fd6fd3834960829c7
Zeeshan-H <zeeshanh184@gmail.com>
2019-12-26 14:08:34 -0800

    Del

---
+++ MRM/Models/MF_StatusModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Sender.Models
+{
+    public class MF_StatusModel
+    {
+        public short MFStatusId { get; set; }
+        public string MEStatus { get; set; }
+
+    }
+}
